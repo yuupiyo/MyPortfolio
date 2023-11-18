0fa615a1dcde99ca06be4f929a4889fc1d251707
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2023-11-18 21:54:32 +0900

    header.php作成

--- single.php
+++ single.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="宮崎県の小さな山村で4人のお母さんをしながら、コーダーを目指す日吉ゆかりのポートフォリオです。">
-  <title>Yukari Hiyoshi's Portfolio</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Zen+Kaku+Gothic+New:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/style.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
-  <script src="./js/main.js" defer></script>
-  <script src="./js/gsap.js" defer></script>
-</head>
-<body>
-  <header class="l-header c-background__glass">
-    <button class="p-hamburger js-hamburger" type="button">
-      <span class="c-background__brown"></span>
-      <span class="c-background__brown"></span>
-      <span class="c-background__brown"></span>
-    </button>
-    <nav class="p-nav js-nav">
-      <ul class="c-flex__nav c-font-family__en c-color__brown">
-        <li><a href="#">Home</a></li>
-        <li><a href="./index.html/#about">About</a></li>
-        <li><a href="./index.html/#contact">Skill</a></li>
-        <li><a href="./index.html/#works">Works</a></li>
-        <li><a href="./index.html/#contact">Contact</a></li>
-      </ul>
-    </nav>
-    <div class="c-background__circle js-circle"></div>
-  </header>
+<?php get_header(); ?>
 
   <main class="l-main p-single" ontouchstart="">
     <div class="p-single__title">
