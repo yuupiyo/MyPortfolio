b348d1f2dc17b73ed89e07f2febde88d32f25b5c
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2023-12-19 00:11:52 +0900

    リファクタリング途中まで完了

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@
       <div class="p-page__text c-text c-font-family__ja c-color__brown">
         <p>お探しのページは、削除されたか、名前が変更された可能性があります。</p>
         <p class="u-mb__24">直接アドレスを入力された場合は、アドレスが正しく入力されているかもう一度ご確認ください。</p>
-        <p>ブラウザの再読み込みを行ってもこのページが表示される場合は、<a class="p-about__span c-font-family__en c-text__buildup" href="<?php echo esc_url(home_url( '/' )); ?>">トップページ</a>から目的のページをお探しください。</p>
+        <p>ブラウザの再読み込みを行ってもこのページが表示される場合は、<a class="c-font-family__en c-text__span c-background__span" href="<?php echo esc_url(home_url( '/' )); ?>">トップページ</a>から目的のページをお探しください。</p>
       </div>
     </div>
   </div>
