b2a8da3491cd2af64ca296ea8bde45053e62f956
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2023-12-25 00:56:29 +0900

    ナビゲーションの動きOK

--- js/gsap.js
+++ js/gsap.js
@@ -20,44 +20,123 @@ const newTextContent = textContent
 paragraph.innerHTML = newTextContent;
 
 
-const texts = document.querySelectorAll('.js-span');
-const mainvisual = document.querySelector('.js-mainvisual');
-const mainLeft = document.querySelector('.js-mainvisualLeft');
-const mainRight = document.querySelector('.js-mainvisualRight');
+// const texts = document.querySelectorAll('.js-span');
+// const mainvisual = document.querySelector('.js-mainvisual');
+// const mainLeft = document.querySelector('.js-mainvisualLeft');
+// const mainRight = document.querySelector('.js-mainvisualRight');
 
-// mainvisual
-gsap.fromTo(texts, 
-{
-  y: 0,
-  autoAlpha: 1,
-},
-{
-  y: 25,
-  autoAlpha: 0,
-  stagger: 0.05,
-  ease: 'power1.out',
-  scrollTrigger: {
-    trigger: mainvisual,
-    start: 'top top',
-    end: 'bottom center',
-    toggleActions: "play none reverse none",
-    scrub: true,
-  },
-});
-gsap.fromTo(mainLeft, 
-  {
-    xPercent: -80,
-  },
-  {
-    xPercent: -40,
-    delay: 0.5,
-    scrollTrigger: {
-      trigger: mainvisual,
-      start: 'top top',
-      toggleActions: "play none reverse none",
-      scrub: true,
+// // mainvisual
+// gsap.fromTo(texts, 
+// {
+//   y: 0,
+//   autoAlpha: 1,
+// },
+// {
+//   y: 25,
+//   autoAlpha: 0,
+//   stagger: 0.05,
+//   ease: 'power1.out',
+//   scrollTrigger: {
+//     trigger: mainvisual,
+//     start: 'top top',
+//     end: 'bottom center',
+//     toggleActions: "play none reverse none",
+//     scrub: true,
+//   },
+// });
+// gsap.fromTo(mainLeft, 
+//   {
+//     xPercent: -80,
+//   },
+//   {
+//     xPercent: -40,
+//     delay: 0.5,
+//     scrollTrigger: {
+//       trigger: mainvisual,
+//       start: 'top top',
+//       toggleActions: "play none reverse none",
+//       scrub: true,
+//     },
+//   });
+//   gsap.fromTo(mainRight, 
+//     {
+//       xPercent: 65,
+//     },
+//     {
+//       xPercent: 30,
+//       delay: 0.5,
+//       scrollTrigger: {
+//         trigger: mainvisual,
+//         start: 'top top',
+//         toggleActions: "play none reverse none",
+//         scrub: true,
+//       },
+//     });
+
+//     // fadein
+//     const fadeinItems =document.querySelectorAll('.js-fadein');
+
+//     fadeinItems.forEach((fadeinItem) => {
+//       gsap.fromTo(fadeinItem, 
+//         {
+//           y: 150,
+//           autoAlpha: 0,
+//         },
+//         {
+//           y: 0,
+//           autoAlpha: 1,
+//           scrollTrigger: {
+//             trigger: fadeinItem,
+//             start: 'top bottom',
+//             end: 'top center',
+//             scrub: true,
+//           },
+//         });
+//     });
+
+const isTopPage = window.location.pathname === '/' || window.location.pathname === '/front-page.php'; // トップページかどうかを判定する例
+
+if (isTopPage) {
+  // トップページの場合のGSAP ScrollTriggerの設定
+  const texts = document.querySelectorAll('.js-span');
+  const mainvisual = document.querySelector('.js-mainvisual');
+  const mainLeft = document.querySelector('.js-mainvisualLeft');
+  const mainRight = document.querySelector('.js-mainvisualRight');
+
+  gsap.fromTo(texts, 
+    {
+      y: 0,
+      autoAlpha: 1,
     },
-  });
+    {
+      y: 25,
+      autoAlpha: 0,
+      stagger: 0.05,
+      ease: 'power1.out',
+      scrollTrigger: {
+        trigger: mainvisual,
+        start: 'top top',
+        end: 'bottom center',
+        toggleActions: "play none reverse none",
+        scrub: true,
+      },
+    });
+
+  gsap.fromTo(mainLeft, 
+    {
+      xPercent: -80,
+    },
+    {
+      xPercent: -40,
+      delay: 0.5,
+      scrollTrigger: {
+        trigger: mainvisual,
+        start: 'top top',
+        toggleActions: "play none reverse none",
+        scrub: true,
+      },
+    });
+
   gsap.fromTo(mainRight, 
     {
       xPercent: 65,
@@ -72,8 +151,6 @@ gsap.fromTo(mainLeft,
         scrub: true,
       },
     });
-
-    // fadein
     const fadeinItems =document.querySelectorAll('.js-fadein');
 
     fadeinItems.forEach((fadeinItem) => {
@@ -93,3 +170,23 @@ gsap.fromTo(mainLeft,
           },
         });
     });
+  } else {
+    // 下層ページの場合の処理
+    // 通常のページ内リンクを使って特定のセクションに移動するためのコードを記述する
+    // ページ内の全てのアンカーリンクを取得
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1); // リンク先のセクションのIDを取得
+        const targetElement = document.getElementById(targetId); // リンク先のセクションの要素を取得
+
+        if (targetElement) {
+          // スクロール処理を実行
+          targetElement.scrollIntoView({
+            behavior: 'smooth' // スムーズなスクロールを有効化
+          });
+        }
+      });
+    });
+  }
