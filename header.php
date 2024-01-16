9444be2c7ad10e109f3183da253300ca8a8d9b1e
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2024-01-17 04:51:08 +0900

    header.phpコード削除

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="<?php bloginfo('description'); ?>">
-  <title><?php bloginfo('name'); ?></title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <?php wp_head(); ?>
